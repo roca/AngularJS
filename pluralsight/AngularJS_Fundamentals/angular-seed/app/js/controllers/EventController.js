9b691562495ea1f70f566e010052f2b9f6313a22
roca <romelcampbell@gmail.com>
2014-05-13 14:05:13 -0400

    Added sessions

--- pluralsight/AngularJS_Fundamentals/angular-seed/app/js/controllers/EventController.js
+++ pluralsight/AngularJS_Fundamentals/angular-seed/app/js/controllers/EventController.js
@@ -12,7 +12,32 @@ eventsApp.controller('EventController',
           		city: 'Mountain View',
           		province: 'CA'
           	},
-          	imageUrl: 'img/AngularJS-large.png'
+          	imageUrl: 'img/AngularJS-large.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives!'
+
+
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: '30 mins',
+                    level: 'Introductory',
+                    abstract: 'In this session you will learn the ins and outs of directives'
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Bob Smith',
+                    duration: '2 hours',
+                    level: 'Intermediate',
+                    abstract: 'This session you take a closer look at scopes. Learn what they do, how they do it. and how to get them to do it.'
+                }
+            ]
           }
 
 });
