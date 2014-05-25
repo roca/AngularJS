0deef570a7618d28882da6e3b525d3d6f15fb11a
roca <RomelCampbell@gmail.com>
2014-05-25 06:02:03 -0400

    Finshed section on Creating Your First Custom Service: moduel #3

--- pluralsight/AngularJS_Fundamentals/angular-seed/app/js/controllers/EventController.js
+++ pluralsight/AngularJS_Fundamentals/angular-seed/app/js/controllers/EventController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 eventsApp.controller('EventController',
-	function EventController($scope, $sce){
+	function EventController($scope, $sce,eventData){
 
         $scope.snippet = '<span style="color:red"> hi there</span>';
         $scope.deliberatelyTrustDangerousSnippet = function() {
@@ -14,45 +14,7 @@ eventsApp.controller('EventController',
         $scope.buttonDisabled = false
         $scope.sortorder = "name";
 
-          $scope.event = {
-          	name: 'Angular Boot Camp',
-          	date: '1/1/2013',
-          	time: '10:30 am',
-          	location: {
-          		address: 'Google Headquarters',
-          		city: 'Mountain View',
-          		province: 'CA'
-          	},
-          	imageUrl: 'img/AngularJS-large.png',
-            sessions: [
-                {
-                    name: 'Directives Masterclass introductory',
-                    creatorName: 'Bob Smith',
-                    duration: 1,
-                    level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!',
-                    upVoteCount: 0
-
-
-                },
-                {
-                    name: 'Scopes for fun and profit',
-                    creatorName: 'John Doe',
-                    duration: 2,
-                    level: 'Introductory',
-                    abstract: 'In this session you will learn the ins and outs of directives',
-                    upVoteCount: 0
-                },
-                {
-                    name: 'Well Behaved Controllers',
-                    creatorName: 'Bob Smith',
-                    duration: 4,
-                    level: 'Intermediate',
-                    abstract: 'This session you take a closer look at scopes. Learn what they do, how they do it. and how to get them to do it.',
-                    upVoteCount: 0
-                }
-            ]
-          }
+          $scope.event = eventData.event;
 
         $scope.toggleShowHide = function(session) {
             $scope.boolValue = !$scope.boolValue;
---
+++ pluralsight/AngularJS_Fundamentals/angular-seed/app/js/services/EventData.js
@@ -0,0 +1,45 @@
+eventsApp.factory('eventData',function() {
+
+    return {
+         event: {
+          	name: 'Angular Boot Camp',
+          	date: '1/1/2013',
+          	time: '10:30 am',
+          	location: {
+          		address: 'Google Headquarters',
+          		city: 'Mountain View',
+          		province: 'CA'
+          	},
+          	imageUrl: 'img/AngularJS-large.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass introductory',
+                    creatorName: 'Bob Smith',
+                    duration: 1,
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
+
+
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: 2,
+                    level: 'Introductory',
+                    abstract: 'In this session you will learn the ins and outs of directives',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Bob Smith',
+                    duration: 4,
+                    level: 'Intermediate',
+                    abstract: 'This session you take a closer look at scopes. Learn what they do, how they do it. and how to get them to do it.',
+                    upVoteCount: 0
+                }
+            ]
+          }
+    };
+
+});
