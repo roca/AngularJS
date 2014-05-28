dd2d6ec876376566d1245043dfbd8c14c66e1c0e
roca <RomelCampbell@gmail.com>
2014-05-27 21:27:24 -0400

    Still trying to save

--- pluralsight/AngularJS_Fundamentals/angular-seed/app/js/controllers/EditEventController.js
+++ pluralsight/AngularJS_Fundamentals/angular-seed/app/js/controllers/EditEventController.js
@@ -2,9 +2,13 @@
 
 eventsApp.controller('EditEventController',
 	function EditEventController($scope,eventData){
+
+		$scope.event = {};
+
 		$scope.saveEvent = function(event,newEventForm) {
 			console.log(newEventForm);
 			if (newEventForm.$valid) {
+
 	            eventData.save(event).$promise.then(
           			function(response){console.log('success',response);},
           			function(response){console.log('failure',response);}
