d593d27ea0ebfcf07fc8d0874f63982d84d25b58
roca <RomelCampbell@gmail.com>
2014-06-12 20:40:51 -0400

    Custom Elements

---
+++ pluralsight/AngularJS_Fundamentals/angular-seed/app/js/directives/mySample.js
@@ -0,0 +1,19 @@
+'use strict';
+
+eventsApp.directive('mySample',function($compile) {
+
+	// return {
+	// 	restrict: 'E',
+	// 	link:  function(scope,element,attrs,controller){
+	// 		var markup = "<input type='text' ng-model='sampleData' />{{sampleData}}<br/>";
+	// 	    angular.element(element).html($compile(markup)(scope));
+	// 	}
+	// };
+	return {
+		restrict: 'E',
+		template:   "<input type='text' ng-model='sampleData' />{{sampleData}}<br/>"
+		//templateUrl: 'templates/EventDetails.html'
+		
+	};
+
+});
