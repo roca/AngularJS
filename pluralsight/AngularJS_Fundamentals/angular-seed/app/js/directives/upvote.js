6791b07073ec1739ed7a200f149b6c2062a32392
roca <RomelCampbell@gmail.com>
2014-06-14 05:12:48 -0400

    fixed the upvote directive

--- pluralsight/AngularJS_Fundamentals/angular-seed/app/js/directives/upvote.js
+++ pluralsight/AngularJS_Fundamentals/angular-seed/app/js/directives/upvote.js
@@ -3,7 +3,13 @@
 eventsApp.directive('upvote',function() {
 	return {
 		restrict: 'E',
-		templateUrl: 'templates/directives/upvote.html'
+		templateUrl: 'templates/directives/upvote.html',
+		scope: {
+			upvote: 	"&",
+			downvote: 	"&",
+			count:		"="
+		}
+		
 	};
 
 });
