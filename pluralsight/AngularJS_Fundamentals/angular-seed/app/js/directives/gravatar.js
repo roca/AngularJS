507947616438a1fb59a33c7ef7ac344fcc59d886
roca <romelcampbell@gmail.com>
2014-06-16 09:57:15 -0400

    Got the gravatar directive to work

--- pluralsight/AngularJS_Fundamentals/angular-seed/app/js/directives/gravatar.js
+++ pluralsight/AngularJS_Fundamentals/angular-seed/app/js/directives/gravatar.js
@@ -7,16 +7,13 @@ eventsApp.directive('gravatar',function(gravatarUrlBuilder) {
 		replace:  true,
 		template: '<img />',
 		link: function(scope, element, attrs, controller) {
-            attrs.$observe('email',function(oldValue,newValue){
-            	if (oldValue !== newValue) {
-            		attrs.$set('src',gravatarUrlBuilder.buildGravatarUrl(newValue));
-            	}
+            attrs.$observe('email',function(value){
+            	attrs.$set('src',gravatarUrlBuilder.buildGravatarUrl(value));
             });
         }
 	};
 
 });
 
-// <img ng-src="{{getGravatarUrl(user.emailAddress)}}"/>
 
 
--- pluralsight/AngularJS_Fundamentals/angular-seed/app/js/services/GravatarUrlBuilder.js
+++ pluralsight/AngularJS_Fundamentals/angular-seed/app/js/services/GravatarUrlBuilder.js
@@ -3,6 +3,7 @@
 eventsApp.factory('gravatarUrlBuilder', function () {
    return {
        buildGravatarUrl: function(email) {
+       
            var defaultGravatarUrl = "http://www.gravatar.com/avatar/000?s=200";
 
            var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
