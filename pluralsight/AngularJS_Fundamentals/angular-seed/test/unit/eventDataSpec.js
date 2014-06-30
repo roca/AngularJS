16e93648a9c4fc2b1b6a47469c4109f85881f8aa
roca <romelcampbell@gmail.com>
2014-06-30 09:29:28 -0400

    Test AJAX Services with

--- pluralsight/AngularJS_Fundamentals/angular-seed/test/unit/eventDataSpec.js
+++ pluralsight/AngularJS_Fundamentals/angular-seed/test/unit/eventDataSpec.js
@@ -1,24 +1,26 @@
 'use strict';
 
-describe('eventData',function($resource){
+describe('eventData',function(){
 
 	var mockEventResource;
 
     beforeEach(module('eventsApp'));
 
-     beforeEach(function(){
-     	mockEventResource = sinon.stub({get: function(){}});
-     	module(function($provide){
-     		$provide.value($resource,mockEventResource);
+    //  beforeEach(function(){
+    //  	mockEventResource = sinon.stub({get: function(){}});
+    //  	module(function($provide){
+    //  		$provide.value('$resource',mockEventResource);
 
-     	})
-    })
+    //  	})
+    // })
 
-     describe('getEvent',function(){
+     describe('get',function(){
 
-	    it('should return the correct Event data',inject(function(eventData){
-	             eventData.getEvent(2);
-	             expect(mockEventResource.get.args[0][0].toEqual({name:"Angular Boot Camp"}));
+	    it("should issue a GET request to '/app/data/event/1.json' when the id is 1",inject(function(eventData,$httpBackend){
+                $httpBackend.when('GET','/app/data/event/1.json').respond({name:"Angular Boot Camp"});
+	             var result = eventData.getEvent(1);
+                 $httpBackend.flush();
+	             expect(result.name).toBe("Angular Boot Camp");
 
 	     }))
 
