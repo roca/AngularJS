06856b7ddd21291efd68816574fb773061ead5e0
roca <romelcampbell@gmail.com>
2014-07-02 09:25:00 -0400

    Testing Filters

---
+++ pluralsight/AngularJS_Fundamentals/angular-seed/test/unit/durationsSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('durations',function(){
+
+
+    beforeEach(module('eventsApp'));
+
+	    it("should return 'Half hour' when given a 1",inject(function(durationsFilter){
+	    	expect(durationsFilter(1)).toEqual('Half hour');
+ 
+	    }));
+});
