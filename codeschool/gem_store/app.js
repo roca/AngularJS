bf478881439bd19377711d81849016ced5ba1fa3
roca <RomelCampbell@gmail.com>
2014-08-09 05:16:02 -0400

    Upto Module 2.1

--- codeschool/gem_store/app.js
+++ codeschool/gem_store/app.js
@@ -1,10 +1,13 @@
 (function() {
+  var app = angular.module('gemStore', []);
 
-	var gem = { name: 'Azurite', price: 2.95 };
-	var app = angular.module('gemStore',[]);
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
 
-	app.controller('StoreController',function(){
-		this.product = gem;
-	});
-
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 },
+  ];
 })();
