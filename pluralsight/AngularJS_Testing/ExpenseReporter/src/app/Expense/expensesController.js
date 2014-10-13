83dc6368df72d95ed7fd5f1d0666b68398eae18b
roca <RomelCampbell@gmail.com>
2014-10-12 21:03:52 -0400

    Finished my first Angular Spec

---
+++ pluralsight/AngularJS_Testing/ExpenseReporter/src/app/Expense/expensesController.js
@@ -0,0 +1,26 @@
+(function(){
+	'use strict'
+
+	angular.module('app')
+		.controller('expensesController',[expensesController]);
+
+
+		function expensesController(){
+
+			var vm = this;
+
+			vm.activate = activate;
+			vm.expenseItems = [];
+
+			activate();
+
+			function activate(){
+
+				return vm.expenseItems = [{title:'Taxi',  	description: 'To airport', amount: 89.95},
+						{title:'Lunch', 	description: 'At airport', amount: 15.40},
+						{title:'Coffee',  	description: 'starbucks',  amount: 4.93}];
+
+			};
+
+		};
+})();
