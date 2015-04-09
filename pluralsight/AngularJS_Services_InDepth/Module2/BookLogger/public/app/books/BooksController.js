17acbaf84266ce2263c3db40235421b53aacde63
Romel Campbell <romel.campbell@regeneron.com>
2015-04-09 09:46:44 -0400

    Finished the 'Demmo: Using the $q Service' in module 3

--- pluralsight/AngularJS_Services_InDepth/Module2/BookLogger/public/app/books/BooksController.js
+++ pluralsight/AngularJS_Services_InDepth/Module2/BookLogger/public/app/books/BooksController.js
@@ -4,70 +4,70 @@
         .controller('BooksController', ['books','dataService','logger','badgeService','$q',BooksController]);
 
 
-    function BooksController(books,dataService,logger,badgeService) {
+    function BooksController(books,dataService,logger,badgeService,$q) {
 
         var vm = this;
 
         vm.appName = books.appName;
 
-        // var booksPromise = dataService.getAllBooks();
-        // var readersPromise = dataService.getAllReaders();
+        var booksPromise = dataService.getAllBooks();
+        var readersPromise = dataService.getAllReaders();
 
-        // $q.all([booksPromise,readersPromise])
-        // .then(getAllDataSuccess)
-        // .catch(getAllDataError);
+        $q.all([booksPromise,readersPromise])
+        .then(getAllDataSuccess)
+        .catch(getAllDataError);
 
-        // function getAllDataSuccess(dataArray) {
-        //     vm.allBooks = dataArray[0];
-        //     vm.allReaders = dataArray[1];
-        // }
-
-        // function getAllDataError (reason) {
-        //     console.log(reason);
-        // }
-
-
-        dataService.getAllBooks()
-            .then(getBooksSuccess,null,getBooksNotifications)
-            .catch(errorCallBack)
-            .finally(getAllBooksComplete);
-
-
-        function getBooksSuccess (books) {
-            //throw('error in the success handler')
-            vm.allBooks = books;
+        function getAllDataSuccess(dataArray) {
+            vm.allBooks = dataArray[0];
+            vm.allReaders = dataArray[1];
         }
 
-        // function getBooksError (reason) {
-        //     console.log(reason);
-        // }
-
-       function errorCallBack(errorMsg) {
-            console.log('Error Message: ' + errorMsg);
-        }
-
-        function getBooksNotifications (notification) {
-            console.log('Promise Notification: ' + notification);
-        }
-
-        function getAllBooksComplete () {
-            console.log('getAllBooks has completed');
+        function getAllDataError (reason) {
+            console.log(reason);
         }
 
 
+       //  dataService.getAllBooks()
+       //      .then(getBooksSuccess,null,getBooksNotifications)
+       //      .catch(errorCallBack)
+       //      .finally(getAllBooksComplete);
 
-        dataService.getAllReaders()
-            .then(getAllReadersSuccess)
-            .catch(errorCallBack)
-            .finally(getAllReadersComplete);
 
-        function getAllReadersSuccess(readers) {
-            vm.allReaders = readers;
-        }
+       //  function getBooksSuccess (books) {
+       //      //throw('error in the success handler')
+       //      vm.allBooks = books;
+       //  }
 
-        function getAllReadersComplete () {
-            console.log('getAllReaders has completed');
-        }
+       //  // function getBooksError (reason) {
+       //  //     console.log(reason);
+       //  // }
+
+       // function errorCallBack(errorMsg) {
+       //      console.log('Error Message: ' + errorMsg);
+       //  }
+
+       //  function getBooksNotifications (notification) {
+       //      console.log('Promise Notification: ' + notification);
+       //  }
+
+       //  function getAllBooksComplete () {
+       //      console.log('getAllBooks has completed');
+       //  }
+
+
+
+       //  dataService.getAllReaders()
+       //      .then(getAllReadersSuccess)
+       //      .catch(errorCallBack)
+       //      .finally(getAllReadersComplete);
+
+       //  function getAllReadersSuccess(readers) {
+       //      vm.allReaders = readers;
+       //  }
+
+       //  function getAllReadersComplete () {
+       //      console.log('getAllReaders has completed');
+       //  }
 
         vm.getBadge = badgeService.retrieveBadge;
 
