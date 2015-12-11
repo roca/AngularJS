cf233d35bf389c3f6ed6a4cf8415c6ca7a4f195e
Romel Campbell <romel.campbell@regeneron.com>
2015-12-11 09:50:55 -0500

    Done creating angular2-quickstart

--- angulario/angular2-quickstart/src/app/app.ts
+++ angulario/angular2-quickstart/src/app/app.ts
@@ -1,11 +1,11 @@
-/// <reference path="typings/angular2/angular2.d.ts" />
+/// <reference path="../../typings/angular2/angular2.d.ts" />
 
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, View, bootstrap} from "angular2/angular2";
 
 
 // Annotation section
 @Component({
-  selector: 'my-app'
+  selector: "my-app"
 })
 @View({
   template: `<h1>Hello {{ name }}</h1><br/> This is my first Angular 2 App`
@@ -15,7 +15,7 @@ class MyAppComponent {
   name: string;
 
   constructor() {
-    this.name = 'All';
+    this.name = "All";
   }
 }
 
